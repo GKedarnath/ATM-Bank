9ca4fc8374eff6059cda1d806e324c8283c37578
GKedarnath <72261336+GKedarnath@users.noreply.github.com>
2022-03-04 17:06:53 +0530

    Add files via upload

--- AtmBank.py
+++ AtmBank.py
@@ -1,16 +1,17 @@
 class AtmBank(object):
-    def __init__(self,accountname,cardnumber,balanceholders,rupees):
+    def __init__(self,accountname,cardnumber,balanceholders,rupees,bankname):
         self.accountname = accountname
         self.cardnumber = cardnumber
         self.balanceholders = balanceholders
         self.rupees = rupees
+        self.bankname = bankname
 
     def deposit(self,rupees):
         print("Today i am going to deposit ",rupees," rupees in the Bank")
     def credit(self,rupees):
         print("Today i am going to credit ",rupees," rupees from the Bank")    
 
-atmbank1 = AtmBank("Kedar",'1357',"4",'50,000,000')    
+atmbank1 = AtmBank("Kedar",'1357',"4",'50,000,000',"AxisBank")    
 
 atmbank1.deposit('5000')
 atmbank1.credit('2500')      
